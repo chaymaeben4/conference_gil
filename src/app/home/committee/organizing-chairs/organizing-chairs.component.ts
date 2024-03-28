bf69d1c405d3c6e26a4fc42e2d6a40f719cd7b63
HBlackfoxx <hamada.cero@gmail.com>
2024-03-28 13:46:39 +0000

    update2
    Chairs fixed
    Scientific-fixed

--- src/app/home/committee/organizing-chairs/organizing-chairs.component.ts
+++ src/app/home/committee/organizing-chairs/organizing-chairs.component.ts
@@ -13,12 +13,12 @@ interface Chair {
 })
 export class OrganizingChairsComponent implements OnInit {
   chairs: Chair[] = [
-    { title: 'General chair', name: 'Mr. BENMOUSSA Rachid', email: 'r.benmoussa@uca.ac' },
+    { title: 'General chair', name: 'Mr. BENMOUSSA Rachid', email: 'r.benmoussa@uca.ma' },
     { title: 'Production chair', name: 'Ms. BENAZZOUZ Touria', email: 't.benazzouz@uca.ma' },
     { title: 'Scientific chair', name: 'Ms. BOUYAHIA Fatima', email: 'f.bouyahia@uca.ac.ma' },
+    { title: 'Financial chair', name: 'Ms. ESSABER Fatima Ezzahra', email: 'essaberfatimaezzahra@gmail.com'},
     { title: 'Logistic chair', name: 'Ms. DAHBI Samia', email: 's.dahbi@uca.ma' },
     { title: 'Logistic chair', name: 'Ms. ELHAROUNI Fatine', email: 'f.elharouni@uca.ac.ma' },
-    { title: 'Financial chair', name: 'Ms. ESSABER Fatima Ezzahra', email: 'essaberfatimaezzahra@gmail.com'},
     { title: 'Industrial chair', name: 'Mr. IRHIRANE Elhassan', email: 'e.irhirane@uca.ac.ma' },
     { title: 'Industrial chair', name: 'Mr. DAKKAK Bader', email: 'b.dakkak@gmail.com' }
   ];
--- src/app/home/committee/scientific/scientific.component.ts
+++ src/app/home/committee/scientific/scientific.component.ts
@@ -6,40 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scientific.component.css']
 })
 export class ScientificComponent implements OnInit {
-  committeeMembers: string[] = [];
+  committeeMembers = [
+    'Ahlaqqach Mustapha | ESITH Casablanca',
+    'El Harraki Imad | ENIM Rabat',
+    'Allaoui Ali | EST Béni Mellal USMS',
+    'El Mhamedi Abderrahman | Paris 8 University',
+    'Allaoui Hamid | University of Artois',
+    'Elassali Raja | ENSA Marrakech, UCA',
+    'Auhmani Khalid | ENSA Marrakech, UCA',
+    'Elfezazi Said | EST SAFI, UCA',
+    'Belhadi Amine | Rabat Business School, UIR',
+    'Elharouni Fatine | ENSA Marrakech, UCA',
+    'Benazzouz Touria | ENSA Marrakech, UCA',
+    'Essaber Fatima Ezzahra | ENSA Marrakech, UCA',
+    'Benhammou Abdelaziz | ENSA Safi, UCA',
+    'Idboufker Noureddine | ENSA Marrakech, UCA',
+    'Benhida Khalid | EST Safi, UCA',
+    'Irhirane El Hassan | ENSA Marrakech, UCA',
+    'Benmoussa Rachid | ENSA Marrakech, UCA',
+    'Laaouina Loubna | ENSA Fez, USMBA',
+    'Bouyahia Fatima | ENSA Marrakech, UCA',
+    'Latif Adnane | ENSA Marrakech, UCA',
+    'Charkaoui Abdelkabir | FST Settat, UH1',
+    'Nejeoui Abderrazzak | ENSA Marrakech, UCA',
+    'Cherrafi Anass | EST Safi, UCA',
+    'Starostka-Patyk Marta | Czestochowa University of Technology',
+    'Dahbi Samya | ENSA Marrakech, UCA',
+    'Tajer Abdelouahed | ENSA Marrakech, UCA',
+    'Dakkak Badr | ENSA Marrakech, UCA',
+    'Touriki Fatima Ezzahra | ENSA SAFI, UCA',
+    'Echchatbi Abdelwahed | FST Settat, UCA'
+  ];
 
-  ngOnInit(): void {
-    // Assuming the data could come from an API or service in a real-world scenario
-    this.committeeMembers = [
-      'Ahlaqqach Mustapha | ESITH Casablanca',
-      'El Harraki Imad | ENIM Rabat',
-      'Allaoui Ali | EST Béni Mellal USMS',
-      'El Mhamedi Abderrahman | Paris 8 University',
-      'Allaoui Hamid | University of Artois',
-      'Elassali Raja | ENSA Marrakech, UCA',
-      'Auhmani Khalid | ENSA Marrakech, UCA',
-      'Elfezazi Said | EST SAFI, UCA',
-      'Belhadi Amine | Rabat Business School, UIR',
-      'Elharouni Fatine | ENSA Marrakech, UCA',
-      'Benazzouz Touria | ENSA Marrakech, UCA',
-      'Essaber Fatima Ezzahra | ENSA Marrakech, UCA',
-      'Benhammou Abdelaziz | ENSA Safi, UCA',
-      'Idboufker Noureddine | ENSA Marrakech, UCA',
-      'Benhida Khalid | EST Safi, UCA',
-      'Irhirane El Hassan | ENSA Marrakech, UCA',
-      'Benmoussa Rachid | ENSA Marrakech, UCA',
-      'Laaouina Loubna | ENSA Fez, USMBA',
-      'Bouyahia Fatima | ENSA Marrakech, UCA',
-      'Latif Adnane | ENSA Marrakech, UCA',
-      'Charkaoui Abdelkabir | FST Settat, UH1',
-      'Nejeoui Abderrazzak | ENSA Marrakech, UCA',
-      'Cherrafi Anass | EST Safi, UCA',
-      'Starostka-Patyk Marta | Czestochowa University of Technology',
-      'Dahbi Samya | ENSA Marrakech, UCA',
-      'Tajer Abdelouahed | ENSA Marrakech, UCA',
-      'Dakkak Badr | ENSA Marrakech, UCA',
-      'Touriki Fatima Ezzahra | ENSA SAFI, UCA',
-      'Echchatbi Abdelwahed | FST Settat, UCA'
-    ];
+  firstColumn: string[] = [];
+  secondColumn: string[] = [];
+
+  ngOnInit() {
+    // Split the array into two columns
+    this.committeeMembers.forEach((member, index) => {
+      if (index % 2 === 0) {
+        this.firstColumn.push(member);
+      } else {
+        this.secondColumn.push(member);
+      }
+    });
   }
 }
+
+
